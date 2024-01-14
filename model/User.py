6dafc7d350efcf5cbe34f2be16ddb097b50db7bf
gorysont <bum3955@gmail.com>
2024-01-14 17:19:34 +0400

    User class has been updated by few methods and atributes

--- model/User.py
+++ model/User.py
@@ -6,21 +6,25 @@ class User:
         self.secondname = ''
         self.middlename = ''
         self.email = ''
+        self.phone = ''
         self.roles = [] 
 
-    def set_firstname(self, value):
+    def set_firstname(self, value: str):
         self.firstname = value
 
-    def set_secondname(self, value):
+    def set_secondname(self, value: str):
         self.secondname = value
 
-    def set_middlename(self, value):
+    def set_middlename(self, value: str):
         self.middlename = value
 
-    def set_email(self, email):
-        self.email = email
+    def set_email(self, value: str):
+        self.email = value
 
-    def add_role(self, value):
+    def set_phone(self, value: str):
+        self.phone = value
+
+    def add_role(self, value: str):
         self.roles.append(value)
 
     def get_firstname(self) -> str:
@@ -31,6 +35,12 @@ class User:
 
     def get_middlename(self) -> str:
         return self.middlename
+
+    def get_email(self) -> str:
+        return self.email
+
+    def get_phone(self) -> str:
+        return self.phone
 
     def get_roles(self) -> list:
         return self.roles
