f3934d176dfe6d8facfbedbf0afd35552cf69809
gorysont <bum3955@gmail.com>
2024-01-14 18:06:32 +0400

    added function for chec if user has role

--- model/User.py
+++ model/User.py
@@ -9,22 +9,22 @@ class User:
         self.phone = ''
         self.roles = [] 
 
-    def set_firstname(self, value: str):
+    def set_firstname(self, value='': str):
         self.firstname = value
 
-    def set_secondname(self, value: str):
+    def set_secondname(self, value='': str):
         self.secondname = value
 
-    def set_middlename(self, value: str):
+    def set_middlename(self, value='': str):
         self.middlename = value
 
-    def set_email(self, value: str):
+    def set_email(self, value='': str):
         self.email = value
 
-    def set_phone(self, value: str):
+    def set_phone(self, value='': str):
         self.phone = value
 
-    def add_role(self, value: str):
+    def add_role(self, value='': str):
         self.roles.append(value)
 
     def get_firstname(self) -> str:
@@ -44,3 +44,19 @@ class User:
 
     def get_roles(self) -> list:
         return self.roles
+
+    def user_has_role(self, value) -> bool:
+        '''Check if user has role or list of roles
+
+        Args:
+            value -- name of role or list of those
+        Returns:
+            bool
+        '''
+        roles = self.get_roles()
+        if value is list:
+            return all([role in roles for role in value])
+        elif value is str:
+            return value in self.get_roles()
+        else:
+            raise TypeError('Value should be string or list')
