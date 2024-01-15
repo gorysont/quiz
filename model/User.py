0d705c5764d67e243b790fcfcf7c0809ccfb7c74
gorysont <bum3955@gmail.com>
2024-01-15 10:20:42 +0400

    change code to decorator's way for setters and getters

--- model/User.py
+++ model/User.py
@@ -7,21 +7,50 @@ class User:
         self.middlename = ''
         self.email = ''
         self.phone = ''
-        self.roles = [] 
+        self.roles = []
 
-    def set_firstname(self, firstname: str = ''):
+    @property
+    def firstname(self) -> str:
+        return self.firstname
+    
+    @property
+    def secondname(self) -> str:
+        return self.secondname
+
+    @property
+    def middlename(self) -> str:
+        return self.middlename
+
+    @property
+    def email(self) -> str:
+        return self.email
+
+    @property
+    def phone(self) -> str:
+        return self.phone
+
+    @property
+    def roles(self) -> list:
+        return self.roles
+
+    @firstname.setter
+    def firstname(self, firstname: str = ''):
         self.firstname = firstname
 
-    def set_secondname(self, secondname: str = ''):
+    @secondname.setter
+    def secondname(self, secondname: str = ''):
         self.secondname = secondname
 
-    def set_middlename(self, middlename: str = ''):
+    @middlename.setter
+    def middlename(self, middlename: str = ''):
         self.middlename = middlename
 
-    def set_email(self, email: str = ''):
+    @email.setter
+    def email(self, email: str = ''):
         self.email = email
 
-    def set_phone(self, phone: str = ''):
+    @phone.setter
+    def phone(self, phone: str = ''):
         self.phone = phone
 
     def add_role(self, rolename: str = ''):
@@ -30,36 +59,12 @@ class User:
     def remove_role(self, rolename: str = ''):
         self.roles.remove(rolename)
 
-    def get_firstname(self) -> str:
-        return self.firstname
+    def has_roles(self, roles_for_check: list = []) -> bool:
+        '''Check if user has roles from list
 
-    def get_secondname(self) -> str:
-        return self.secondname
-
-    def get_middlename(self) -> str:
-        return self.middlename
-
-    def get_email(self) -> str:
-        return self.email
-
-    def get_phone(self) -> str:
-        return self.phone
-
-    def get_roles(self) -> list:
-        return self.roles
-
-    def user_has_role(self, value) -> bool:
-        '''Check if user has role or list of roles
-
-        Args:
-            value -- name of role or list of those
+        Keyword arguments:
+            roles_for_check -- list of role names (default [])
         Returns:
             bool
         '''
-        roles = self.get_roles()
-        if value is list:
-            return all([role in roles for role in value])
-        elif value is str:
-            return value in self.get_roles()
-        else:
-            raise TypeError('Value should be string or list')
+        return all([role in self.roles for role in roles_for_check])
